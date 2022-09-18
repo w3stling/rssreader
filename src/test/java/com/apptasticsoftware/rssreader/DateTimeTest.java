978a5f6c5cbed99c47f4f61a19b49c42e35ed43f
w3stling <peter.westling@gmail.com>
2022-09-18 10:29:19 +0200

    Datetime format not recognized

--- src/test/java/com/apptasticsoftware/rssreader/DateTimeTest.java
+++ src/test/java/com/apptasticsoftware/rssreader/DateTimeTest.java
@@ -32,12 +32,43 @@ class DateTimeTest {
     void dateTimeFormat4() {
         Long timestamp = DateTime.toEpochMilli("Sat, 30 Nov 2019 08:21:14 GMT");
         assertEquals(Long.valueOf(1575102074000L), timestamp);
+
+        timestamp = DateTime.toEpochMilli("Wed, 02 Oct 2002 13:00:00 GMT");
+        assertEquals(Long.valueOf(1033563600000L), timestamp);
+
+        timestamp = DateTime.toEpochMilli("Wed, 02 Oct 2002 15:00:00 +0200");
+        assertEquals(Long.valueOf(1033563600000L), timestamp);
     }
 
     @Test
     void dateTimeFormat5() {
         Long timestamp = DateTime.toEpochMilli("2021-11-17T13:21:21Z");
         assertEquals(Long.valueOf(1637155281000L), timestamp);
+    }
+
+    @Test
+    void dateTimeFormat6() {
+        Long timestamp = DateTime.toEpochMilli("Sun, 04 Sep 2022 09:42:16");
+        assertEquals(Long.valueOf(1662284536000L), timestamp);
+
+        timestamp = DateTime.toEpochMilli("Sun, 4 Sep 2022 09:42:16");
+        assertEquals(Long.valueOf(1662284536000L), timestamp);
+    }
+
+    @Test
+    void dateTimeFormat7() {
+        //https://datatracker.ietf.org/doc/html/rfc4287#section-3.3
+        Long timestamp = DateTime.toEpochMilli("2003-12-13T18:30:02Z");
+        assertEquals(Long.valueOf(1071340202000L), timestamp);
+
+        timestamp = DateTime.toEpochMilli("2003-12-13T18:30:02.25Z");
+        assertEquals(Long.valueOf(1071340202250L), timestamp);
+
+        timestamp = DateTime.toEpochMilli("2003-12-13T18:30:02+01:00");
+        assertEquals(Long.valueOf(1071336602000L), timestamp);
+
+        timestamp = DateTime.toEpochMilli("2003-12-13T18:30:02.25+01:00");
+        assertEquals(Long.valueOf(1071336602250L), timestamp);
     }
 
     @Test
