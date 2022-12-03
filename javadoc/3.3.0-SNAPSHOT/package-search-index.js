b0d0323eea54fa799645ea39ec2dd22311a99f05
w3stling <w3stling@users.noreply.github.com>
2022-12-03 10:38:38 +0000

    Publishing javadoc

---
+++ javadoc/3.3.0-SNAPSHOT/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"com.apptasticsoftware.rssreader","l":"com.apptasticsoftware.rssreader"},{"m":"com.apptasticsoftware.rssreader","l":"com.apptasticsoftware.rssreader.util"}]
