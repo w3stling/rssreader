5074ef2f7d88185549140c003c4d2d2d1a5cbd0d
w3stling <w3stling@users.noreply.github.com>
2023-06-01 21:41:20 +0000

    Publishing javadoc

---
+++ javadoc/3.4.5/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"com.apptasticsoftware.rssreader","l":"com.apptasticsoftware.rssreader"},{"m":"com.apptasticsoftware.rssreader","l":"com.apptasticsoftware.rssreader.module.itunes"},{"m":"com.apptasticsoftware.rssreader","l":"com.apptasticsoftware.rssreader.util"}]
