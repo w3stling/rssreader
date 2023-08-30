123c4f43d6e1a40137ee51cf029b055cc174846b
w3stling <peter.westling@gmail.com>
2023-08-30 17:28:32 +0200

    Fixed code smell

--- src/test/java/com/apptasticsoftware/rssreader/util/MapperTest.java
+++ src/test/java/com/apptasticsoftware/rssreader/util/MapperTest.java
@@ -9,11 +9,11 @@ import static com.apptasticsoftware.rssreader.util.Mapper.mapInteger;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MapperTest {
+class MapperTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"true", "TRUE", "True", "yes", "YES", "Yes"})
-    public void testMapBooleanTrue(String trueValue) {
+    void testMapBooleanTrue(String trueValue) {
         Item item = new Item(new DateTime());
         Mapper.mapBoolean(trueValue, item::setIsPermaLink);
         assertEquals(true, item.getIsPermaLink().orElse(null));
@@ -21,7 +21,7 @@ public class MapperTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"false", "FALSE", "False", "no", "NO", "No"})
-    public void testMapBooleanFalse(String falseValue) {
+    void testMapBooleanFalse(String falseValue) {
         Item item = new Item(new DateTime());
         Mapper.mapBoolean(falseValue, item::setIsPermaLink);
         assertEquals(false, item.getIsPermaLink().orElse(null));
@@ -29,7 +29,7 @@ public class MapperTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "-1", "0", "12345", "-12345"})
-    public void testMapInt(String intTextValue) {
+    void testMapInt(String intTextValue) {
         Image image = new Image();
         Mapper.mapInteger(intTextValue, image::setHeight);
         assertEquals(Integer.valueOf(intTextValue), image.getHeight().orElse(null));
@@ -37,7 +37,7 @@ public class MapperTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"aaa", "a1", "1a"})
-    public void testMapBadInt(String intTextValue) {
+    void testMapBadInt(String intTextValue) {
         Image image = new Image();
         Mapper.mapInteger(intTextValue, image::setHeight);
         assertTrue(image.getHeight().isEmpty());
@@ -45,7 +45,7 @@ public class MapperTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "-1", "0", "12345", "-12345"})
-    public void testMapLong(String longTextValue) {
+    void testMapLong(String longTextValue) {
         Enclosure enclosure = new Enclosure();
         Mapper.mapLong(longTextValue, enclosure::setLength);
         assertEquals(Long.valueOf(longTextValue), enclosure.getLength().orElse(null));
@@ -53,7 +53,7 @@ public class MapperTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"aaa", "a1", "1a"})
-    public void testMapBadLong(String longTextValue) {
+    void testMapBadLong(String longTextValue) {
         Enclosure enclosure = new Enclosure();
         Mapper.mapLong(longTextValue, enclosure::setLength);
         assertTrue(enclosure.getLength().isEmpty());
@@ -61,7 +61,7 @@ public class MapperTest {
 
 
     @Test
-    public void testCreateIfNull() {
+    void testCreateIfNull() {
         Channel channel = new Channel(new DateTime());
         Mapper.createIfNull(channel::getImage, channel::setImage, Image::new).setTitle("title");
         assertEquals("title", channel.getImage().map(Image::getTitle).orElse("-"));
@@ -71,7 +71,7 @@ public class MapperTest {
     }
 
     @Test
-    public void testCreateIfNullOptional() {
+    void testCreateIfNullOptional() {
         Channel channel = new Channel(new DateTime());
         Mapper.createIfNullOptional(channel::getImage, channel::setImage, Image::new).ifPresent(i -> mapInteger("200", i::setHeight));
         assertEquals(200, channel.getImage().flatMap(Image::getHeight).orElse(0));
