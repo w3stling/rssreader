e5f56e9e74a4b100aa81f8db0c325b2a6f5f0396
Peter Westling <w3stling@users.noreply.github.com>
2024-11-24 08:41:37 +0100

    Add content field to item (#195)

--- src/test/java/com/apptasticsoftware/rssreader/RssReaderTest.java
+++ src/test/java/com/apptasticsoftware/rssreader/RssReaderTest.java
@@ -507,8 +507,21 @@ class RssReaderTest {
 
     @Test
     void equalsContract() {
-        EqualsVerifier.simple().forClass(Channel.class).withIgnoredFields("dateTimeParser").withIgnoredFields("category").withIgnoredFields("syUpdatePeriod").withIgnoredFields("syUpdateFrequency").withNonnullFields("categories").verify();
-        EqualsVerifier.simple().forClass(Item.class).withIgnoredFields("defaultComparator").withIgnoredFields("dateTimeParser").withIgnoredFields("category").withNonnullFields("categories").withIgnoredFields("enclosure").withNonnullFields("enclosures").verify();
+        EqualsVerifier.simple().forClass(Channel.class)
+                .withIgnoredFields("dateTimeParser")
+                .withIgnoredFields("category")
+                .withIgnoredFields("syUpdatePeriod")
+                .withIgnoredFields("syUpdateFrequency")
+                .withNonnullFields("categories")
+                .verify();
+        EqualsVerifier.simple().forClass(Item.class)
+                .withIgnoredFields("defaultComparator")
+                .withIgnoredFields("dateTimeParser")
+                .withIgnoredFields("category")
+                .withNonnullFields("categories")
+                .withIgnoredFields("enclosure")
+                .withNonnullFields("enclosures")
+                .verify();
         EqualsVerifier.simple().forClass(Enclosure.class).verify();
         EqualsVerifier.simple().forClass(Image.class).verify();
     }
