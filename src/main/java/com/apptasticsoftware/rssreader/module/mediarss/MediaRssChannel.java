958e52a488ee33ea238f30917cfa631445973635
w3stling <peter.westling@gmail.com>
2025-11-29 22:23:21 +0100

    Refactor to use with interfaces

--- src/main/java/com/apptasticsoftware/rssreader/module/mediarss/MediaRssChannel.java
+++ src/main/java/com/apptasticsoftware/rssreader/module/mediarss/MediaRssChannel.java
@@ -2,5 +2,5 @@ package com.apptasticsoftware.rssreader.module.mediarss;
 
 import com.apptasticsoftware.rssreader.Channel;
 
-public interface MediaRssChannel extends Channel, MediaRssChannelData {
+public interface MediaRssChannel extends MediaRssChannelData, Channel {
 }
