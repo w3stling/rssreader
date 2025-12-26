2227ad09cd107620c73f9272edc264becc61f0ea
w3stling <peter.westling@gmail.com>
2025-12-26 12:02:37 +0100

    Fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -32,7 +32,6 @@ module com.apptasticsoftware.rssreader {
 
     exports com.apptasticsoftware.rssreader;
     exports com.apptasticsoftware.rssreader.util;
-    exports com.apptasticsoftware.rssreader.module.georss;
     exports com.apptasticsoftware.rssreader.module.itunes;
     exports com.apptasticsoftware.rssreader.module.mediarss;
     exports com.apptasticsoftware.rssreader.module.opensearch;
