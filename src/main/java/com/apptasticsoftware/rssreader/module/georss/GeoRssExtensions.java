1548711f25c480304b2c30c7b37290467f4f59b7
w3stling <peter.westling@gmail.com>
2025-12-26 18:34:27 +0100

    Add test

--- src/main/java/com/apptasticsoftware/rssreader/module/georss/GeoRssExtensions.java
+++ src/main/java/com/apptasticsoftware/rssreader/module/georss/GeoRssExtensions.java
@@ -2,10 +2,13 @@ package com.apptasticsoftware.rssreader.module.georss;
 
 import com.apptasticsoftware.rssreader.FeedExtensionRegistry;
 
+import java.util.List;
+
 import static com.apptasticsoftware.rssreader.util.Mapper.mapDouble;
 import static com.apptasticsoftware.rssreader.util.Mapper.mapInteger;
 
 public class GeoRssExtensions {
+    private static final List<String> ITEM_PATHS = List.of("/rss/channel/item/", "/feed/entry/");
 
     public static void register(FeedExtensionRegistry<? extends GeoRssChannel, ? extends GeoRssItem> registry) {
         itemTagExtensions(registry);
@@ -22,5 +25,17 @@ public class GeoRssExtensions {
         registry.addItemExtension("georss:featuretypetag", GeoRssItemData::setGeoRssFeatureTypeTag);
         registry.addItemExtension("georss:relationshiptag", GeoRssItemData::setGeoRssRelationshipTag);
         registry.addItemExtension("georss:featurename", GeoRssItemData::setGeoRssFeatureName);
+
+        registry.addItemExtension(ITEM_PATHS, "georss:where/gml:Point/gml:pos", GeoRssItemData::setGeoRssPoint);
+        registry.addItemExtension(ITEM_PATHS, "georss:where/gml:LineString/gml:posList", GeoRssItemData::setGeoRssLine);
+        registry.addItemExtension(ITEM_PATHS, "georss:where/gml:Polygon/gml:exterior/gml:LinearRing/gml:posList", GeoRssItemData::setGeoRssPolygon);
+        registry.addItemExtension(ITEM_PATHS, "georss:where/gml:Envelope/gml:lowerCorner", GeoRssExtensions::mapEnvelope);
+        registry.addItemExtension(ITEM_PATHS, "georss:where/gml:Envelope/gml:upperCorner", GeoRssExtensions::mapEnvelope);
+    }
+
+    private static void mapEnvelope(GeoRssItem geoRssItem, String text) {
+        var existingValue = geoRssItem.getGeoRssBox().orElse("");
+        var newValue = existingValue.trim() + " " + text.trim();
+        geoRssItem.setGeoRssBox(newValue);
     }
 }
--- src/test/java/com/apptasticsoftware/rssreader/module/georss/GeoRssFeedReaderTest.java
+++ src/test/java/com/apptasticsoftware/rssreader/module/georss/GeoRssFeedReaderTest.java
@@ -1,6 +1,5 @@
 package com.apptasticsoftware.rssreader.module.georss;
 
-import com.apptasticsoftware.rssreader.filter.InvalidXmlCharacterFilter;
 import com.apptasticsoftware.rssreader.module.georss.internal.GeoRssChannelImpl;
 import com.apptasticsoftware.rssreader.module.georss.internal.GeoRssItemDataImpl;
 import com.apptasticsoftware.rssreader.module.georss.internal.GeoRssItemImpl;
@@ -21,7 +20,7 @@ class GeoRssFeedReaderTest {
 
     @Test
     void example1() {
-        var items = new GeoRssFeedReader().addFeedFilter(new InvalidXmlCharacterFilter()).read(fromFile("georss/example1.xml"))
+        var items = new GeoRssFeedReader().read(fromFile("georss/example1.xml"))
                 .collect(Collectors.toList());
 
         assertEquals(1, items.size());
@@ -54,6 +53,38 @@ class GeoRssFeedReaderTest {
         assertThat(item.getGeoRssFeatureTypeTag(), isPresentAndIs("city"));
         assertThat(item.getGeoRssRelationshipTag(), isPresentAndIs("is-centered-at"));
         assertThat(item.getGeoRssFeatureName(), isPresentAndIs("Podunk"));
+    }
+
+    @Test
+    void example2() {
+        var items = new GeoRssFeedReader().read(fromFile("georss/example2.xml"))
+                .collect(Collectors.toList());
+
+        assertEquals(1, items.size());
+        var item = items.get(0);
+
+        var channel = (GeoRssChannel) item.getChannel();
+        assertThat(channel.getTitle(), is("Earthquakes"));
+        assertThat(channel.getLink(), is("http://example.org/"));
+        assertThat(channel.getLastBuildDate(), isPresentAndIs("2005-12-13T18:30:02Z"));
+        assertThat(channel.getLastBuildDateAsZonedDateTime(), isPresentAndIs(Default.getDateTimeParser().parse("2005-12-13T18:30:02Z")));
+        assertThat(channel.getManagingEditor(), isPresentAndIs("Dr. Thaddeus Remor"));
+
+        assertThat(item.getTitle(), isPresentAndIs("M 3.2, Mona Passage"));
+        assertThat(item.getLink(), isPresentAndIs("http://example.org/2005/09/09/atom01"));
+        assertThat(item.getGuid(), isPresentAndIs("urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a"));
+        assertThat(item.getPubDate(), isPresentAndIs("2005-08-17T07:02:32Z"));
+        assertThat(item.getPubDateAsZonedDateTime(), isPresentAndIs(Default.getDateTimeParser().parse("2005-08-17T07:02:32Z")));
+        assertThat(item.getUpdated(), isPresentAndIs("2005-08-17T07:02:32Z"));
+        assertThat(item.getUpdatedAsZonedDateTime(), isPresentAndIs(Default.getDateTimeParser().parse("2005-08-17T07:02:32Z")));
+        assertThat(item.getGeoRssPoint(), isPresentAndIs("45.257 -71.93"));
+        assertThat(item.getGeoRssPointAsCoordinate(), isPresentAndIs(new Coordinate(45.257, -71.93)));
+        assertThat(item.getGeoRssLine(), isPresentAndIs("45.256 -110.45 46.46 -109.48 43.84 -109.86"));
+        assertThat(item.getGeoRssLineAsCoordinates(), is(List.of(new Coordinate(45.256, -110.45), new Coordinate(46.46, -109.48), new Coordinate(43.84, -109.86))));
+        assertThat(item.getGeoRssPolygon(), isPresentAndIs("45.256 -110.45 46.46 -109.48 43.84 -109.86 45.256 -110.45"));
+        assertThat(item.getGeoRssPolygonAsCoordinates(), is(List.of(new Coordinate(45.256, -110.45), new Coordinate(46.46, -109.48), new Coordinate(43.84, -109.86), new Coordinate(45.256, -110.45))));
+        assertThat(item.getGeoRssBox(), isPresentAndIs("42.943 -71.032 43.039 -69.856"));
+        assertThat(item.getGeoRssBoxAsCoordinates(), is(List.of(new Coordinate(42.943, -71.032), new Coordinate(43.039, -69.856))));
     }
 
     @Test
