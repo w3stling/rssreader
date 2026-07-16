77816aa25f9bb8cfeb5f415a2b6196c8e94ea576
w3stling <peter.westling@gmail.com>
2026-07-16 08:17:14 +0200

    Fix code smell

--- src/test/java/com/apptasticsoftware/rssreader/RssReaderTest.java
+++ src/test/java/com/apptasticsoftware/rssreader/RssReaderTest.java
@@ -4,7 +4,6 @@ import com.apptasticsoftware.rssreader.internal.ChannelImpl;
 import com.apptasticsoftware.rssreader.internal.ItemImpl;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
